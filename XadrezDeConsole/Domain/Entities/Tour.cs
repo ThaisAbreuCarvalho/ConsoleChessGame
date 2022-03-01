54ab9425b4169ae211c38e12d1937f45a2203224
Thais Carvalho <thaisabreucarvalho@gmail.com>
2022-03-01 19:31:12 -0300

    Revert "Movimentation rules for king and tour"

--- XadrezDeConsole/Domain/Entities/Tour.cs
+++ XadrezDeConsole/Domain/Entities/Tour.cs
@@ -12,50 +12,9 @@ namespace XadrezDeConsole.Domain.Entities
         {
         }
 
-        public bool IsMovementValid(Position position)
-        {
-            var hasPiece = this.Board.Piece(position);
-
-            if (hasPiece == null || hasPiece.Color != this.Color)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
         public override bool[,] PossibleMovements()
         {
-            var response = new bool[this.Board.Lines, this.Board.Columns];
-            var currentPosition = this.Board.Piece(this.Position);
-
-            //upper
-            for (int i = currentPosition.Position.Line; i > 0; i--)
-            {
-                var isEnemy = this.Board.Piece(new Position(i, currentPosition.Position.Column));
-
-                if (isEnemy != null && isEnemy.Color == this.Color && isEnemy.Position != currentPosition.Position)
-                {
-                    break;
-                }
-                else if (isEnemy.Position != currentPosition.Position)
-                {
-                    response[i, currentPosition.Position.Column] = true;
-                }
-            }
-
-            //left side
-            for (int i = currentPosition.Position.Line; i > 0; i--)
-            {
-                var isEnemy = this.Board.Piece(new Position(currentPosition.Position.Line, i));
-
-                if (isEnemy == null || isEnemy.Color != this.Color && isEnemy.Position != currentPosition.Position)
-                {
-                    response[i, currentPosition.Position.Column] = true;
-                }
-            }
-
-            return response;
+            throw new NotImplementedException();
         }
 
         public override string ToString()
